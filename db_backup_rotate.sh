dbefacfef3b2f302c936ec8aaa584ea25c2317fb
Travis Foster <travis@ddv.com>
2015-01-29 14:45:08 -0800

    dump to tmp_dir then move to storage_dir helpful if storage_dir is NFS mount and tmp_dir is local

--- db_backup_rotate.sh
+++ db_backup_rotate.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-dump_dir="/tmp"
+umask 0077
+
+storage_dir="~"
+tmp_dir="/tmp"
 dump_file="db.dump"
 gzip=1
-dumps_to_keep=10
+dumps_to_keep=5
 
 prefix=""
 if [ $gzip ]
@@ -18,7 +21,7 @@ MYSQL_DB="dbname"
 MYSQL_HOST="dbhost"
 MYSQL_PORT=3306
 
-mysqldump -u ${MYSQL_USER} -p${MYSQL_PASS} -P${MYSQL_PORT} -h ${MYSQL_HOST} ${MYSQL_DB} >> ${dump_dir}/${dump_file}.tmp || {
+mysqldump -u ${MYSQL_USER} -p${MYSQL_PASS} -P${MYSQL_PORT} -h ${MYSQL_HOST} ${MYSQL_DB} >> "${tmp_dir}/${dump_file}.tmp" || {
   echo "Dump command failed"
   exit 1
 } 
@@ -27,12 +30,12 @@ mysqldump -u ${MYSQL_USER} -p${MYSQL_PASS} -P${MYSQL_PORT} -h ${MYSQL_HOST} ${MY
 while [ $dumps_to_keep -ne 1 ]
 do
   let dump_to_mv=$dumps_to_keep-1
-  old_file="${dump_dir}/${dump_file}.${dump_to_mv}${suffix}"
-  new_file="${dump_dir}/${dump_file}.${dumps_to_keep}${suffix}"
+  old_file="${storage_dir}/${dump_file}.${dump_to_mv}${suffix}"
+  new_file="${storage_dir}/${dump_file}.${dumps_to_keep}${suffix}"
 
-  if [ -e ${dump_dir}/${dump_file}.${dump_to_mv}${suffix} ]
+  if [ -e "${storage_dir}/${dump_file}.${dump_to_mv}${suffix}" ]
   then
-    mv $old_file $new_file || {
+    mv "$old_file" "$new_file" || {
       echo "Could not move $old_file to $new_file"
       exit 1
     }
@@ -40,8 +43,8 @@ do
   let dumps_to_keep=$dumps_to_keep-1
 done
 
-mv ${dump_dir}/${dump_file}.tmp ${dump_dir}/${dump_file}.1
+mv "${tmp_dir}/${dump_file}.tmp" "${storage_dir}/${dump_file}.1"
 if [ $gzip ]
 then
-  gzip $dump_dir/$dump_file.1
+  gzip "${storage_dir}/$dump_file.1"
 fi
