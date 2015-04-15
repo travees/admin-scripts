61e8ff7fb55e1f89ffbd6b6ee392b8bf59a9a00d
Travis Foster <travis@ddv.com>
2015-04-15 16:14:06 -0700

    Read all socket data before close() so that the TCP connection isn't reset, and buffered data will be sent.

--- stillserver.py
+++ stillserver.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+#
+# Single threaded sockets server:
+# - Listen for connections
+# - Grab image from HTTP source.
+# - Send image to connected clients.
+#
+# Sending relies on socket.sendall(). This is a blocking process but we rely on TCP buffers 
+# to accept the data immediately since our amount of data is small. It would probably be 
+# better to loop over the connections and send a chunk of data until all has been sent.
+#
 
 import sys
 import socket
@@ -19,8 +29,12 @@ base64string = base64.encodestring(
 authHeader =  "Basic %s" % base64string
 requestString = "GET /SnapshotJPEG?Resolution=320x240&Quality=Standard\n\n"
 
-DEBUG = 0
+DEBUG = 1
 
+def debug(msg):
+    if DEBUG:
+        sys.stdout.write(msg)
+        sys.stdout.flush()
 
 def startServer(ip, port):
     _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,49 +48,40 @@ serverSocket = startServer('0.0.0.0', 8888)
 
 read_list = [serverSocket]
 write_list = []
+
 while True:
     readable, writable, errored = select.select(read_list, [], [])
     for s in readable:
-        if s is serverSocket:
-            client_socket, address = serverSocket.accept()
-            read_list.append(client_socket)
-            #print "Connection from", address
-            write_list.append(client_socket)
+        client_socket, address = serverSocket.accept()
+        client_socket.shutdown(socket.SHUT_RD)
+        debug("Connection: %s\n" % (address,))
+        write_list.append(client_socket)
     readable, writable, errored = select.select([], write_list, [], 0)
     if len(writable) > 0:
-        if DEBUG:
-            sys.stdout.write('w')
-            sys.stdout.flush()
         req = urllib2.Request('http://10.1.1.104/SnapshotJPEG?Resolution=320x240&Quality=Standard')
         req.add_header("Authorization", authHeader)
         response = urllib2.urlopen(req)
         headers = ''.join(response.headers.headers)
         img = response.read()
     else:
-        if DEBUG:
-            sys.stdout.write('p')
-            sys.stdout.flush()
-        pass
-
+        continue
 
     for s in writable:
-        if DEBUG:
-            sys.stdout.write('s')
-            sys.stdout.flush()
         try:
-            s.sendall("HTTP/1.0 200 OK\n");
-            s.sendall(''.join((headers,"\n",img)))
-            s.close()
-            write_list.remove(s)
-            read_list.remove(s)
-            if DEBUG:
-                sys.stdout.write('S')
-                sys.stdout.flush()
-        except:
-            s.close()
-            print "Connection closed", address
-            write_list.remove(s)
-            read_list.remove(s)
+            s.sendall("HTTP/1.0 200 OK\n"+''.join((headers,"\n",img)))
+            s.shutdown(socket.SHUT_RDWR)
             
-
+            # Consume all received data so that when we close() it doesn't tear down the connection.
+            # If there is buffered data to read, close() will kill the connection even if there is 
+            # buffered data to send to the client.
+            while client_socket.recv(1024, socket.MSG_DONTWAIT):
+                pass
+            s.close()
+            
+            write_list.remove(s)
+        except Exception,e:
+            print "Error: %s" % (e,)
+            print "Connection closed: %s" % (s,)
+            s.close()
+            write_list.remove(s)
     
